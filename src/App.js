34420bd934bd7d2b87510aaf37d0eb437a4236fc
obibaadoma <akrasikonadu@yahoo.com>
2023-08-11 12:03:03 +0000

    fixed linters and added logic and readme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Calculator from '../components/Calculator';
+import Calculator from 'Calculator.js';
 
 class App extends React.Component {
   constructor(props) {
