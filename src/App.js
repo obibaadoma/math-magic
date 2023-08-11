4c046f940dcac0950eafdcb8b03105871ae0a5bf
obibaadoma <akrasikonadu@yahoo.com>
2023-08-11 14:47:14 +0000

    fixed readme and linters

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
-import Calculator from './components/Calculator';
-
+import Calculator from './components/calculator';
 class App extends React.Component {
   constructor(props) {
     super(props);
