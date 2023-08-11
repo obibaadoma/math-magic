a1dd34b3965fa8648bf93c9ee4e37bb5edbce16b
obibaadoma <akrasikonadu@yahoo.com>
2023-08-11 19:16:48 +0000

    fixed linters in hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import Calculator from './components/Calculator';
+import Calculator from './components/calculator';
 import './components/styles.css';
 
 function App() {
